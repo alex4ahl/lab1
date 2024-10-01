bf8a876d68b6acc14d25d740f738a1903a7b1985
alex4ahl <84011500+alex4ahl@users.noreply.github.com>
2024-10-01 09:52:43 +0200

    Add files via upload

---
+++ Lab1/test.py
@@ -0,0 +1,25 @@
+import wordfreq
+
+def run_tests():
+    document = ['"Här är BAJEN! Sköna BAJEN, sjung för HAMMARBY! Här är BAJEN, vafan är ni? Vill ni va som HAMMARBY? Störst och flest det vet', 
+                'alla om, glasen höjs och så ekar vår sång! Här är BAJEN, vafan är ni, VI ÄR HAMMARBY!!!" Sade de mest visa folkgrupper från söder.', 
+                " "]
+
+    tokens = wordfreq.tokenize(document)
+    print("Tokens: ",   tokens)
+
+    print("")
+
+    stopWords = ['är', 'de', 'heja', '!', "här", "sjung", "vafan", "vill", "och", "vet", "vår", "ni", "?", "va", "som", "sade", "de", '"', "för" , ",", ".", "vi", "visa", "mest", "om", "alla", "det", "så", "från"]
+    word_count = wordfreq.countWords(tokens, stopWords)
+
+    #print("Word count without stopwords: ")
+    #wordfreq.countWords(tokens, stopWords)
+
+    print ("")
+
+    print("Top 5 words:")
+    wordfreq.printTopMost(word_count, n=5)
+
+
+run_tests()
